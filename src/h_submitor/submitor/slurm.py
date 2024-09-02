37f8238f6cedf00d494916e0ef51893ad06184f8
Roy Kid <lijichen365@gmail.com>
2024-09-02 08:35:04 +0000

    adapt caching; job deps

--- src/h_submitor/submitor/slurm.py
+++ src/h_submitor/submitor/slurm.py
@@ -17,6 +17,7 @@ class SlurmSubmitor(BaseSubmitor):
         account: str | None = None,
         script_name: str | Path = "run_slurm",
         test_only: bool = False,
+        job_deps: list[str] | dict[str, list[str]] | None = None,
         **slurm_kwargs,
     ) -> int:
         submit_config = slurm_kwargs.copy()
@@ -31,6 +32,19 @@ class SlurmSubmitor(BaseSubmitor):
             submit_config["--chdir"] = work_dir
         if account:
             submit_config["--account"] = account
+        if job_deps:
+            self.monitor.refresh_all()
+            job_mapping = {name: self.monitor.get_status_by_name(name) for name in job_deps}
+            if isinstance(job_deps, list):
+                submit_config["--dependency"] = "afterok:" + ":".join(
+                    map(str, job_mapping.values())
+                )
+            elif isinstance(job_deps, dict):
+                condition = job_deps.pop("condition", ",")  # default to AND
+                submit_config["--dependency"] = condition.join(
+                    f"{k}:{':'.join(map(str, [job_mapping[j] for j in v]))}"
+                    for k, v in job_deps.items()
+                )
 
         script_path = self._gen_script(Path(script_name), cmd, **submit_config)
 
@@ -72,7 +86,7 @@ class SlurmSubmitor(BaseSubmitor):
     def query(self, job_id: int) -> JobStatus:
         cmd = ["squeue", "-j", str(job_id)]
         proc = subprocess.run(cmd, capture_output=True)
-        header, job = proc.stdout.split("\n")
+        header, job = proc.stdout.decode().split("\n")[:2]
         status = {k: v for k, v in zip(header.split(), job.split())}
         if status["ST"] == "R":
             enum_status = "RUNNING"
@@ -95,7 +109,7 @@ class SlurmSubmitor(BaseSubmitor):
 
     def validate_config(self, config: dict) -> dict:
         return super().validate_config(config)
-    
+
     def cancel(self, job_id: int):
         cmd = ["scancel", str(job_id)]
         proc = subprocess.run(cmd, capture_output=True)
